5605b7945db822d458e2a38526f5a81fcd432a72
Daniel Venable <daniel.t.venable@gmail.com>
2022-04-15 09:25:19 -0700

    Keeps track of who is on stage

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 import { readFile } from 'fs/promises';
 
-const text = String(await readFile('trigram.txt'));
+const text = String(await readFile('trigram.txt')),
+    characterText = String(await readFile('characters.txt'));
 
-const chunks = text.split(/\n/);
+const chunks = text.split('\n');
+const characters = new Map(characterText.split('\n').map(a => a.split('#')));
 
 function* words(text) {
     let [w1, w2] = text.split(/\s/).slice(-2);
@@ -13,7 +15,7 @@ function* words(text) {
 }
 
 function getWord(w1, w2) {
-    const text = `${w1} ${w2} `;
+    const text = `${w1} ${w2} `.toLowerCase();
     const index = binarySearch(chunks, text);
     let stop, start;
     for (stop = index + 1; chunks[stop].startsWith(text); stop++);
@@ -22,7 +24,6 @@ function getWord(w1, w2) {
 }
 
 function binarySearch(arr, text) {
-    text = text.toLowerCase();
     let start = 0;
     let end = arr.length - 1;
 
@@ -45,14 +46,61 @@ function random(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-const gen = words('THE END');
+const totalCharacters = [...characters].reduce((acc, [, num]) => acc + +num, 0);
+
+function randomCharacter(exclude) {
+    const totalAllowed = [...exclude].reduce((acc, cur) => acc - characters.get(cur), totalCharacters);
+    let index = random(0, totalAllowed);
+    for (const [character, num] of characters) {
+        if (exclude.has(character)) continue;
+        index -= num;
+        if (index < 0) return character;
+    }
+}
+
+const gen = words('Act I.');
+const onStage = new Set();
+let talking;
 
 for (let i = 0; i < 1000; i++) {
     const word = gen.next().value;
-    if (/[A-Z]+[.]/.test(word)) {
-        process.stdout.write('\n');
+    if (word === '#') {
+        onStage.delete(talking);
+        if (onStage.size === 0) enter();
+        const character = [...onStage][random(0, onStage.size)];
+        if (talking !== undefined) onStage.add(talking);
+        talking = character;
+        process.stdout.write('\n' + character);
+    } else if (word === '>') {
+        enter();
+    } else if (word === '<') {
+        process.stdout.write('\nExit ');
+        let counter = 0;
+        for (const num = random(0, onStage.size); counter < num; counter++) {
+            const character = [...onStage][random(0, onStage.size)];
+            onStage.delete(character);
+            process.stdout.write(character.slice(0, -1) + (num === 1 ? ' ' : ', '));
+        }
+        if (counter) process.stdout.write('and ');
+        const character = [...onStage][random(0, onStage.size)];
+        onStage.delete(character);
+        process.stdout.write(character);
+        if (character === talking) talking = undefined;
     } else {
-        process.stdout.write(' ');
+        process.stdout.write(' ' + word);
     }
-    process.stdout.write(word);
+}
+
+function enter() {
+    process.stdout.write('\nEnter ');
+    let counter = 0;
+    for (const num = random(0, Math.max(5 - onStage.size, 0)); counter < num; counter++) {
+        const character = randomCharacter(onStage);
+        onStage.add(character);
+        process.stdout.write(character.slice(0, -1) + (num === 1 ? ' ' : ', '));
+    }
+    if (counter) process.stdout.write('and ');
+    const character = randomCharacter(onStage);
+    onStage.add(character);
+    process.stdout.write(character);
 }
--- n-gram-loader.js
+++ n-gram-loader.js
@@ -2,17 +2,47 @@ import { readFile } from 'fs/promises';
 import { createWriteStream } from 'fs';
 
 const text = String(await readFile('plays.txt'));
-const words = text.split(/\s\s?/);
-const chunks = [];
+const reg = /(?<=\n)(?!SCENE|ACT|EPILOGUE|PROLOGUE|M\.)[A-Z ]+\./g;
 
-for (let i = 0; i <= words.length - 3; i++) {
-    chunks.push(`${words[i].toLowerCase()} ${words[i + 1].toLowerCase()} ${words[i + 2]}\n`);
+{
+    console.log('Loading trigram...');
+    const words = text
+        .replace(reg, '#')
+        .replace(/(?<=\n)(Enter|Re-enter) .+?(?=\n#)/gs, '>')
+        .replace(/\[?<?(Exit|Exeunt).+?(?=\n[#>])/gs, '<')
+        .split(/\s/);
+    const chunks = [];
+
+    for (let i = 0; i <= words.length - 3; i++) {
+        chunks.push(`${words[i].toLowerCase()} ${words[i + 1].toLowerCase()} ${words[i + 2]}\n`);
+    }
+
+    chunks.sort((a, b) => a.localeCompare(b));
+
+    const stream = createWriteStream('trigram.txt');
+
+    for (const chunk of chunks) {
+        stream.write(chunk);
+    }
 }
 
-chunks.sort((a, b) => a.localeCompare(b));
+{
+    console.log('Loading characters...');
+    const characters = new Map;
+    for (const character of text.match(reg)) {
+        characters.set(character, (characters.get(character) ?? 0) + 1);
+    }
 
-const stream = createWriteStream('trigram.txt');
-
-for (const chunk of chunks) {
-    stream.write(chunk);
+    const stream = createWriteStream('characters.txt');
+    
+    let isFirst = true;
+    for (const [character, num] of characters) {
+        if (isFirst) {
+            isFirst = false;
+            stream.write(`${character}#${num}`);
+            continue;
+        }
+        stream.write(`\n${character}#${num}`);
+    }
 }
+console.log('Done!');
